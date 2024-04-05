574a1aa59036561b848c4ab886cc2163a02f0a63
taco <taco@taco.taco>
2024-04-05 14:03:25 +0300

    us news works perfectly, but other countries dont

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from newsapi import NewsApiClient
+import json
+import pandas as pd
+#from pandas.io.json import json_normalize
 import config
+from random import randint
+
+#articleList = [(randint(1, 100)) for i in range(10)]
 
 newsapi = NewsApiClient(api_key=config.API)
 
+top_headlines = newsapi.get_top_headlines(category='business')
+#print(type(top_headlines))
+#print(top_headlines)
+sources = newsapi.get_sources()
+
+df = pd.DataFrame(list(top_headlines.items()), columns=['title', 'author'])
 
 
-
+for i in range(5):
+    randNum = (randint(0, 10))
+    print(top_headlines['articles'][randNum]['title'])
+    print(top_headlines['articles'][randNum]['source']['name'])
 
 
