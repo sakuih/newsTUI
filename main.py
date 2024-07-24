4cd957ccbb0ffd86a2388f5112f9d10313e44fcc
sakuih <taco@debian.debian.com>
2024-07-25 01:48:41 +0300

    pivot from NewsApiClient to beautifulsoup4

--- main.py
+++ main.py
@@ -1,25 +1,58 @@
-from newsapi import NewsApiClient
-import json
-import pandas as pd
+#import json
+#import pandas as pd
 #from pandas.io.json import json_normalize
-import config
-from random import randint
-
-#articleList = [(randint(1, 100)) for i in range(10)]
-
-newsapi = NewsApiClient(api_key=config.API)
-
-top_headlines = newsapi.get_top_headlines(category='business')
-#print(type(top_headlines))
-#print(top_headlines)
-sources = newsapi.get_sources()
-
-df = pd.DataFrame(list(top_headlines.items()), columns=['title', 'author'])
+#import config
+#from random import randint
+import requests
+from bs4 import BeautifulSoup
 
 
-for i in range(5):
-    randNum = (randint(0, 10))
-    print(top_headlines['articles'][randNum]['title'])
-    print(top_headlines['articles'][randNum]['source']['name'])
+hsUrl = 'https://www.hs.fi'
+alJazeeraUrl = 'https://www.aljazeera.com'
+jerusalemUrl = 'https://www.jpost.com'
+hindustanUrl = 'https://www.hindustantimes.com'
+scmpUrl = 'https://www.scmp.com'
+
+
+
+def getaljazeera():
+    response = requests.get(aljazeeraUrl)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    func(soup)
+
+def geths():
+    response = requests.get(hsUrl)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    results = soup.body.section.span
+    func(results)
+
+def getjerusalem():
+    response = requests.get(jerusalemUrl)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    func(soup)
+
+def getHidustan():
+    response = requests.get(hindustanUrl)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    func(soup)
+
+def getScmp():
+    response = requests.get(scmpUrl)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    func(soup)
+
+def func (results):
+    #headings = soup.find_all('span')
+    for result in results:
+        print(result.text.strip())
+
+geths()
+#getHidustan()
+#getScmp()
+#getaljazeera()
+#getjerusalem()
+
+
+
 
 
